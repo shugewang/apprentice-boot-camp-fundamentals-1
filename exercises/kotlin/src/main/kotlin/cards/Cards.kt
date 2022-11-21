b8af43c417697c3ef9196a9748bfff0a7c0a3107
Shu Wang <shu.wang@autotrader.co.uk>
2022-11-21 14:27:10 +0000

    Add PlayingCard class

--- exercises/kotlin/src/main/kotlin/cards/Cards.kt
+++ exercises/kotlin/src/main/kotlin/cards/Cards.kt
@@ -1,30 +1,33 @@
 package cards
 
+class PlayingCard (val suit: Int, val faceValue: Int) {
+}
+
 class Cards {
     val cards: MutableList<String>
         get() {
             val result = MutableList(52) {""}
-            val deck = Array(52) { IntArray(2) }
+            val deck = Array<PlayingCard?>(52) {null}
             for (suit in 0..3) {
                 for (faceValue in 0..12) {
-                    deck[suit * 13 + faceValue] = intArrayOf(suit, faceValue)
+                    deck[suit * 13 + faceValue] = PlayingCard(suit, faceValue)
                 }
             }
             for ((cardNumber, card) in deck.withIndex()) {
-                val faceValueName: String = when (card[1]) {
+                val faceValueName: String = when (card?.faceValue) {
                     0 -> "ace"
-                    1, 2, 3, 4, 5, 6, 7, 8, 9 -> (card[1] + 1).toString()
+                    1, 2, 3, 4, 5, 6, 7, 8, 9 -> (card.faceValue + 1).toString()
                     10 -> "jack"
                     11 -> "queen"
                     12 -> "king"
-                    else -> throw IllegalArgumentException("Something went wrong " + card[1] + "is not a valid faceValue!")
+                    else -> throw IllegalArgumentException("Something went wrong " + card?.faceValue + "is not a valid faceValue!")
                 }
-                val suitName: String = when (card[0]) {
+                val suitName: String = when (card.suit) {
                     0 -> "clubs"
                     1 -> "diamonds"
                     2 -> "hearts"
                     3 -> "spades"
-                    else -> throw IllegalArgumentException("Something went wrong " + card[0] + "is not a valid suitName!")
+                    else -> throw IllegalArgumentException("Something went wrong " + card.suit + "is not a valid suitName!")
                 }
                 result[cardNumber] = "$faceValueName of $suitName"
             }
