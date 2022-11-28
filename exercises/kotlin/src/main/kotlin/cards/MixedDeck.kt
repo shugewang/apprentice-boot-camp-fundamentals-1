d7b823d47b907e6721302d2e4d05f5e9ac5a3b06
Shu Wang <shu.wang@autotrader.co.uk>
2022-11-28 10:50:59 +0000

    Added mixed deck to snap game

--- exercises/kotlin/src/main/kotlin/cards/MixedDeck.kt
+++ exercises/kotlin/src/main/kotlin/cards/MixedDeck.kt
@@ -1,7 +1,7 @@
 package cards
 
-class MixedDeck(val animalDeck: AnimalDeck, val playingCardDeck: PlayingCardDeck): Deck(
-    (((animalDeck.deck as ArrayList<Card> + playingCardDeck.deck as ArrayList<Card>) as ArrayList<Card>))
+class MixedDeck(animalDeck: AnimalDeck, playingCardDeck: PlayingCardDeck): Deck(
+    ((animalDeck.deck + playingCardDeck.deck) as ArrayList<Card>)
 ) {
 
 }
